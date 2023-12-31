10f72619b6ce7fca44f952162b37c77d487ac789
Malek H. <119138451+mhammoud-os@users.noreply.github.com>
2023-12-30 19:31:41 -0500

    Added Expetion if coordinits are out of bound

--- json_file_to_jsonl.py
+++ json_file_to_jsonl.py
@@ -15,11 +15,15 @@ nameAdd = "gs://cloud-ai-platform-ef85cba5-03a6-46a2-8ac8-2f8fcd138294/"
 ResourceLabels = {"aiplatform.googleapis.com/annotation_set_name": "6974310007122690048"}
 
 # These are the width and height of the images
-width = 640
-height = 640
+width = 416
+height = 416
 
 newData = []
 
+def check_out_bound(data):
+    if data<0: return 0
+    elif data>1: return 1
+    else: return data
 # This is the format needed
 # {"imageGcsUri":"gs://cloud-ai-platform-ef85cba5-03a6-46a2-8ac8-2f8fcd138294/resized011a_jpg.rf.188f0f379bee36325155637889ea7c0e.jpg","boundingBoxAnnotations":[{"displayName":"weed","xMin":0.2315202231520223,"xMax":0.8131101813110181,"yMin":0.17852161785216178,"yMax":0.7782426778242678,"annotationResourceLabels":{"aiplatform.googleapis.com/annotation_set_name":"6974310007122690048"}}],"dataItemResourceLabels":{}}
 for i in fileData:
@@ -35,6 +39,10 @@ for i in fileData:
         yMin = (y - h / 2) / height
         xMax = (x + w / 2) / width
         yMax = (y + h / 2) / height
+        xMin = check_out_bound(xMin)
+        yMin = check_out_bound(yMin)
+        xMax = check_out_bound(xMax)
+        yMax = check_out_bound(yMax)
 
         line["boundingBoxAnnotations"].append(
             {"displayName": j["label"], "xMin": xMin, "xMax": xMax, "yMin": yMin, "yMax": yMax,
